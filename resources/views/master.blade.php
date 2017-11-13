de5bee6ead0462d2925486c127aa38040e8ef04c
Hugo Burguete <hugoburguete@hotmail.com>
2017-11-13 14:59:30 +0000

    indented master layout

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,32 +11,32 @@
     <link href="{{ url('/css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<div id="app">
+    <div id="app">
         <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="{{ url('/') }}">Home</a></li>
-              <li><a href="{{ route('contact-message.index') }}">Messages</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-		<div class="content row">
-			<div class="col-md-6 col-md-offset-3">
-				@yield('content')
-			</div>
-		</div>
-	</div>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="{{ url('/') }}">Home</a></li>
+                        <li><a href="{{ route('contact-message.index') }}">Messages</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div><!--/.container-fluid -->
+        </nav>
+        <div class="content row">
+            <div class="col-md-6 col-md-offset-3">
+                @yield('content')
+            </div>
+        </div>
+    </div>
     <script src="{{ url('/js/app.js') }}"></script>
 </body>
 </html>
